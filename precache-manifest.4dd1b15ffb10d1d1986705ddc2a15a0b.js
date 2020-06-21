3f4d1d943ad163fe3b1561357c72e508cafd34dd
kajal.karmakar <kajal.karmakar@theonetechnologies.co.in>
2020-06-21 21:17:34 +0530

    Updates

---
+++ precache-manifest.4dd1b15ffb10d1d1986705ddc2a15a0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c204978d38978fc94fa7309905b6ce0f",
+    "url": "/reactJs_kajal/index.html"
+  },
+  {
+    "revision": "9a1b08f4d855eb4384e1",
+    "url": "/reactJs_kajal/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "36f037ca2a3c7fd8e117",
+    "url": "/reactJs_kajal/static/js/2.b072d574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactJs_kajal/static/js/2.b072d574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a1b08f4d855eb4384e1",
+    "url": "/reactJs_kajal/static/js/main.5c14d096.chunk.js"
+  },
+  {
+    "revision": "f8288ed1b42be7a03b7d",
+    "url": "/reactJs_kajal/static/js/runtime-main.2caf2fa0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactJs_kajal/static/media/logo.5d5d9eef.svg"
+  }
+]);
