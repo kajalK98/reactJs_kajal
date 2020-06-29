cfb9ab20451704b0b21e996d97df0974d214e770
kajal.karmakar <kajal.karmakar@theonetechnologies.co.in>
2020-06-29 11:54:40 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactJs_kajal/precache-manifest.5937243d5d73a47e8793fecaefaea8d4.js"
+  "/reactJs_kajal/precache-manifest.4c515c7bad17af5a645f256686b14cd4.js"
 );
 
 self.addEventListener('message', (event) => {
