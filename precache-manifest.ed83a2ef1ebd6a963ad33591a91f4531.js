583d741867560df5fa0996ddf875f47f1217b79a
kajal.karmakar <kajal.karmakar@theonetechnologies.co.in>
2020-07-01 20:02:48 +0530

    Updates

---
+++ precache-manifest.ed83a2ef1ebd6a963ad33591a91f4531.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281ed003d78ec8a299bedffc0251579c",
+    "url": "/reactJs_kajal/index.html"
+  },
+  {
+    "revision": "578afa8472a8489f787c",
+    "url": "/reactJs_kajal/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ece475caf381ee16e3cb",
+    "url": "/reactJs_kajal/static/css/main.26d9e6fb.chunk.css"
+  },
+  {
+    "revision": "578afa8472a8489f787c",
+    "url": "/reactJs_kajal/static/js/2.522f3f6a.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/reactJs_kajal/static/js/2.522f3f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece475caf381ee16e3cb",
+    "url": "/reactJs_kajal/static/js/main.cc36d7bb.chunk.js"
+  },
+  {
+    "revision": "a48fe29bed5b28238078",
+    "url": "/reactJs_kajal/static/js/runtime-main.33b6f8f5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactJs_kajal/precache-manifest.302d166008eed8543f37861237e8b02d.js"
+  "/reactJs_kajal/precache-manifest.ed83a2ef1ebd6a963ad33591a91f4531.js"
 );
 
 self.addEventListener('message', (event) => {
